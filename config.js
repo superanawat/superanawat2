522817f0c734795850cbf54cef86e5b76e4c2850
superanawat <35832942+superanawat@users.noreply.github.com>
2026-06-30 15:50:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 const CONFIG = {
     // นำ URL ที่ได้จาก ngrok มาใส่ที่นี่ (เปลี่ยนใหม่ทุกครั้งที่รัน ngrok ใหม่)
     // หมายเหตุ: ใช้ https:// และไม่ต้องมีเครื่องหมาย / ต่อท้าย
-    API_URL: "https://0ec4-182-52-236-56.ngrok-free.app" 
+    API_URL: "https://6ae8-182-52-236-56.ngrok-free.app" 
 };
