dca0a0485875fd3c4dcf6131c8f64ea8ecebda9d
superanawat <35832942+superanawat@users.noreply.github.com>
2026-07-04 12:00:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,8 @@
 // config.js
 
-//const CONFIG = {
+const CONFIG = {
     // นำ URL ที่ได้จาก ngrok มาใส่ที่นี่ (เปลี่ยนใหม่ทุกครั้งที่รัน ngrok ใหม่)
     // หมายเหตุ: ใช้ https:// และไม่ต้องมีเครื่องหมาย / ต่อท้าย
-    //API_URL: "https://tipped-roast-tamale.ngrok-free.dev" 
-//};
-// config.js
-const CONFIG = {
-    API_URL: "https://tipped-roast-tamale.ngrok-free.app",
-    SOCKET_OPTIONS: {
-        transports: ["websocket", "polling"],
-        extraHeaders: {
-            "ngrok-skip-browser-warning": "true"
-        }
-    }
+    API_URL: "https://tipped-roast-tamale.ngrok-free.dev" 
 };
+
