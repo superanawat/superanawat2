3c5fe4c24d904d2cbb290e9a98db34f821a673b9
superanawat <35832942+superanawat@users.noreply.github.com>
2026-07-06 14:02:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 const CONFIG = {
     // นำ URL ที่ได้จาก ngrok มาใส่ที่นี่ (เปลี่ยนใหม่ทุกครั้งที่รัน ngrok ใหม่)
     // หมายเหตุ: ใช้ https:// และไม่ต้องมีเครื่องหมาย / ต่อท้าย
-    API_URL: [
+    API_URLS: [
         "https://36d9-182-52-236-56.ngrok-free.app",
         "https://tipped-roast-tamale.ngrok-free.dev"
         ]
