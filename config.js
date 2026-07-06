8db2442ccc4e227d9320035e4a25876dfaadf08b
superanawat <35832942+superanawat@users.noreply.github.com>
2026-07-06 13:51:36 +0700

    Update LB config.js

--- config.js
+++ config.js
@@ -3,5 +3,7 @@
 const CONFIG = {
     // นำ URL ที่ได้จาก ngrok มาใส่ที่นี่ (เปลี่ยนใหม่ทุกครั้งที่รัน ngrok ใหม่)
     // หมายเหตุ: ใช้ https:// และไม่ต้องมีเครื่องหมาย / ต่อท้าย
-    API_URL: "https://36d9-182-52-236-56.ngrok-free.app" 
+    API_URL: [
+        "https://36d9-182-52-236-56.ngrok-free.app",
+        "https://tipped-roast-tamale.ngrok-free.dev"
 };
