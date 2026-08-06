cc74a56d760694afde78b143e3d04628bfdfbf0a
superanawat <35832942+superanawat@users.noreply.github.com>
2026-08-06 17:45:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 const CONFIG = {
     // นำ URL ที่ได้จาก ngrok มาใส่ที่นี่ (เปลี่ยนใหม่ทุกครั้งที่รัน ngrok ใหม่)
     // หมายเหตุ: ใช้ https:// และไม่ต้องมีเครื่องหมาย / ต่อท้าย
-    API_URL: "https://tipped-roast-tamale.ngrok-free.dev",
+    API_URL: "https://tipped-roast-tamale.ngrok-free.dev"//,
     
     // เพิ่ม Token สำหรับยืนยันตัวตน (ต้องตั้งให้ตรงกับฝั่ง Python)
-    AUTH_TOKEN: "BigBossBeBelleMayularTL"
+    //AUTH_TOKEN: "BigBossBeBelleMayularTL"
 };
